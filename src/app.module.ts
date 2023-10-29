bdf06be9cd884b4b4be6728bf60e57f61fa1eb88
hqkhai1636 <hqkhai1636@gmail.com>
2023-10-29 15:47:30 +0700

    log every request and response to file and third-party service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ActorModule } from './actor/actor.module';
 import { FilmModule } from './film/film.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './logging/logging.interceptor';
 
 @Module({
   imports: [
@@ -12,6 +14,12 @@ import { ConfigModule } from '@nestjs/config';
     ActorModule,
     FilmModule,
     PrismaModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
+import 'winston-daily-rotate-file';
+import { Loggly } from 'winston-loggly-bulk';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%.log`,
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          maxSize: 100000,
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}:${info.message}`;
+            }),
+          ),
+        }),
+        new Loggly({
+          token: '916a10ca-755f-43e5-944b-a1aa693e5d4c',
+          subdomain: 'localhost',
+          tags: ['Winston-Loggly'],
+          json: true,
+        }),
+      ],
+    }),
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
